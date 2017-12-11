071b2881d74af523ea5477dd75e1411afc643743
clarknoah <noahbc08@gmail.com>
2017-12-11 15:56:46 -0500

    Finished main page routing

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../service/auth.service';
+
 
 @Component({
   selector: 'login',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loggedIn: boolean;
+  message: string;
+  constructor(private auth: AuthService) { }
 
   ngOnInit() {
+  }
+
+  login(user, pass): boolean{
+    this.message = "";
+    if(!this.auth.login(user, pass)){
+      this.message = "Bad Creds";
+      setTimeout(function(){
+        this.message = "";
+
+      }.bind(this),2500);
+    }
+    return false;
+  }
+
+  logout():boolean{
+    this.auth.logout();
+    return false;
   }
 
 }
