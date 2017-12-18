4d3d75ab3dfb646d5b5170e668dd115596011d48
clarknoah <noahbc08@gmail.com>
2017-12-18 12:26:05 -0500

    closed FormBuilder and Validation

--- src/app/dependency-injection/dependency-injection.component.ts
+++ src/app/dependency-injection/dependency-injection.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FactoryService} from '../service/factory.service';
+import {SingletonService} from '../service/singleton.service';
+import {ise_variable} from '../service/variable.service';
 
 @Component({
   selector: 'dependency-injection',
@@ -7,7 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DependencyInjectionComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(
+    private singletonOne: SingletonService,
+    private factoryOne: FactoryService,
+    private singletonTwo: SingletonService,
+    private factoryTwo: FactoryService) { }
 
   ngOnInit() {
   }
--- src/app/validation/validation.component.ts
+++ src/app/validation/validation.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
+import {FormBuilder, FormGroup, FormControl, Validators, AbstractControl} from '@angular/forms';
+
 @Component({
   selector: 'validation',
   templateUrl: './validation.component.html',
   styleUrls: ['./validation.component.css']
 })
 export class ValidationComponent implements OnInit {
+  form:FormGroup;
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      regular_string:['', Validators.compose([Validators.required])],
+      custom_string:['', Validators.compose([Validators.required,
+        this.stringValidator
+      ])],
+      custom_number:['', Validators.compose([Validators.required,
+        this.numberValidator
+      ])]
+    })
 
-  constructor() { }
+  }
 
   ngOnInit() {
+  }
+
+  stringValidator(field:FormControl): {[s:string]:boolean}{
+    if(!field.value.match(/^cat/)){
+      return {invalidField:true};
+    }
+  }
+
+  numberValidator(field:FormControl): {[s:string]:boolean}{
+    if(field.value !==42){
+      return {invalidField:true};
+    }
   }
 
 }
