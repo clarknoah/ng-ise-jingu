160b72ba0fbf8db0c5f0257b2d4d4a8015b75999
clarknoah <noahbc08@gmail.com>
2017-12-22 12:57:43 -0500

    Done with IO

--- src/app/input-output/input-output.component.ts
+++ src/app/input-output/input-output.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import * as _ from 'lodash';
+
 @Component({
   selector: 'input-output',
   templateUrl: './input-output.component.html',
@@ -9,9 +10,12 @@ export class InputOutputComponent implements OnInit {
   initialArray: string[];
   finalArray: string[];
   io1: string[];
+  io1Ready:boolean;
+  io2Ready:boolean;
   io2: string[];
   constructor() {
-
+    this.io1Ready = false;
+    this.io2Ready = false;
   }
 
   ngOnInit() {
@@ -21,13 +25,21 @@ export class InputOutputComponent implements OnInit {
     console.log(this.initialArray);
   }
 
+  setBoolean(key){
+    this[key] = true;
+  }
+
   getIo1(event){
     console.log(event);
+    console.log(this.io1);
     this.io1 = _.cloneDeep(event);
+  //  this.setBoolean('io1Ready');
+    //this.io1Ready = true;
   }
 
   getIo2(event){
     this.finalArray = event;
+  //  this.setBoolean('io2Ready');
   }
 
 }
