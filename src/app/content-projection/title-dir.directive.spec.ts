ae7f2e84da6d618314e7fe451f578342a3e9acce
clarknoah <noahbc08@gmail.com>
2018-02-05 12:05:42 -0500

    Clean slate

--- src/app/content-projection/title-dir.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TitleDirDirective } from './title-dir.directive';
-
-describe('TitleDirDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TitleDirDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/service/variable.service.ts
+++
@@ -1,1 +0,0 @@
-export const ise_variable = "test VARIABLE";
