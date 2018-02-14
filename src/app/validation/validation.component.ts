b29fa8577c08bf03fe195f5d553c8082da4aaf9d
clarknoah <noahbc08@gmail.com>
2018-02-14 14:16:32 -0500

    just in case

--- src/app/validation/validation.component.ts
+++ src/app/validation/validation.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'validation',
   templateUrl: './validation.component.html',
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ValidationComponent implements OnInit {
 
-  constructor() { }
+  stringInput: FormControl = new FormControl(null,Validators.compose([Validators.required]));
+  customStringInput:FormControl;
+  customNumberInput: FormControl;
+  constructor() {
+    this.customStringInput = new FormControl('',Validators.compose([
+      Validators.required, this.customStringValidation
+    ]));
+
+    this.customNumberInput = new FormControl(null,Validators.compose([
+      Validators.required, this.customNumberValidation
+    ]))
+  }
 
   ngOnInit() {
+  }
+
+  customStringValidation(field:FormControl):{[s: string]:boolean}{
+      var strVal: string;
+      strVal = field.value;
+      console.log(field.value);
+      if(!strVal.match(/^cat/)){
+        return {invalidField: true};
+      }
+  }
+
+  customNumberValidation(field:FormControl):{[s: string]:boolean}{
+      if(field.value < 10){
+        return {invalidField: true};
+      }
   }
 
 }
